3d72c9c4dc8985bce22d3bccf9d301b3d1c4ed63
gabrielrguarini <gabrielrguarini@gmail.com>
2022-12-28 17:36:36 -0300

    badges

--- src/components/Pokedex/PokedexPage.tsx
+++ src/components/Pokedex/PokedexPage.tsx
@@ -1,6 +1,5 @@
-import { Grid } from "@mui/material";
+import { Grid, Container, Badge } from "@mui/material";
 import { useContext } from "react";
-import { Container } from "react-bootstrap";
 import { PokedexContext } from "../../contexts/PokedexContext";
 import { PokemonCard } from "../PokemonCard/PokemonCard";
 export function PokedexPage() {
@@ -13,11 +12,16 @@ export function PokedexPage() {
                     {pokemons.pokemons.map((pokemon) => {
                         return (
                             <Grid item xs={12} sm={6} md={3} lg={2}>
-                                <PokemonCard
-                                    key={pokemon.pokemon}
-                                    name={pokemon.pokemon}
-                                    url={`https://pokeapi.co/api/v2/pokemon/${pokemon.pokemon}`}
-                                />
+                                <Badge
+                                    badgeContent={pokemon.count}
+                                    color="primary"
+                                >
+                                    <PokemonCard
+                                        key={pokemon.pokemon}
+                                        name={pokemon.pokemon}
+                                        url={`https://pokeapi.co/api/v2/pokemon/${pokemon.pokemon}`}
+                                    />
+                                </Badge>
                             </Grid>
                         );
                     })}
